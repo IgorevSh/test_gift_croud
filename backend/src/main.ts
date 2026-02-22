dc2ec87b09c6e705140630029a2c3a4afa350ef1
Danila <shumd100@gmail.com>
2026-02-22 19:43:20 +0400

    refactoring

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,11 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new IoAdapter(app));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  const corsOrigin = process.env.CORS_ORIGIN;
-  app.enableCors({
-    origin:"*",
-    credentials: true,
-  });
   const port = parseInt(process.env.PORT || '3001', 10);
   await app.listen(port);
   console.log(`Backend running at http://localhost:${port}`);
